fe3b5645199f7a19890c293c1a06827413755f73
salauddin-sifat-qp <salauddin.sifat@questionpro.com>
2024-07-02 12:23:32 +0600

    update build system

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,39 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
+import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import { name } from "./package.json";
 
+const formattedName = name.match(/[^/]+$/)?.[0] ?? name;
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
     },
   },
   build: {
     sourcemap: true,
     lib: {
-      entry: "src/index.ts",
-      name: "wickui",
-      fileName: (format) => `${format}/index.js`,
-      formats: ["es", "cjs"],
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: formattedName,
+      fileName: (format) => `${formattedName}/${format}/index.js`,
+      formats: ["es", "umd"],
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ["react", "react/jsx-runtime", "react-dom", "tailwindcss"],
       output: {
         globals: {
           react: "React",
+          "react/jsx-runtime": "react/jsx-runtime",
           "react-dom": "ReactDOM",
+          tailwindcss: "tailwindcss",
         },
       },
     },
