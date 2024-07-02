c2cd56deec13f9f7269d5364f24de1f13abb454e
salauddin-sifat-qp <salauddin.sifat@questionpro.com>
2024-07-02 11:28:35 +0600

    bug fix for path alias

--- src/components/button/WuButton.tsx
+++ src/components/button/WuButton.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/ui/button";
+import { Button } from "../../ui/button";
 import { WuButtonSizeEnum } from "./types/WuButtonSizeEnum";
 import { WuButtonVariantEnum } from "./types/WuButtonVariantEnum";
 export interface IWuButtonProps
